e1233e6de263f5c259a247fe6226dff58725d430
Patrick Malouin <p.malouin@gmail.com>
2025-12-05 11:28:02 -0500

    chore(commitlint): bump body-max-line-length from 100 to 500 (#325)
    * chore: reformat commitlin.config.js
    
    * chore(commitlint): bump `body-max-line-length` from 100 to 500
    
    Make it so that dependabot commits are accepted.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,24 @@
+const { RuleConfigSeverity } = require("@commitlint/types");
+
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    //   TODO Add Scope Enum Here
-    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-    'type-enum': [
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 500],
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'feat',
-        'fix',
-        'docs',
-        'chore',
-        'style',
-        'refactor',
-        'ci',
-        'test',
-        'revert',
-        'perf',
-        'vercel',
+        "feat",
+        "fix",
+        "docs",
+        "chore",
+        "style",
+        "refactor",
+        "ci",
+        "test",
+        "revert",
+        "perf",
+        "vercel",
       ],
     ],
   },
